ce42b834394c02f1e53fcb62ba7a389125bebfc1
鱼泡 <jiangtao.yjt@alibaba-inc.com>
2019-10-02 00:07:23 +0800

    不玩了

--- r02_auto_answer.py
+++ r02_auto_answer.py
@@ -6,8 +6,8 @@ from fuzzywuzzy import fuzz
 from selenium import webdriver
 
 # %%  基本参数
-# url = 'https://ks.wjx.top/jq/45065890.aspx'
-url = 'https://ks.wjx.top/jq/45065834.aspx'
+url = 'https://ks.wjx.top/jq/45065890.aspx'
+# url = 'https://ks.wjx.top/jq/45065834.aspx'
 name = '刘彬'
 company = '分公司本部'
 # %% 加载答案
@@ -18,6 +18,26 @@ ans_map = {
     '√': '对', 'x': '错', 'X': '错', 'ｘ': '错', '×': '错'
 }
 # %% 相关操作的抽象
+
+
+def get_single_ans(q, answers):
+    """ 返回的是答案列表 """
+    ratio = answers['题干'].apply(lambda s: fuzz.ratio(s, q))
+    res = answers.loc[ratio.idxmax(), :]
+    ans = res['试题答案']
+    a_list = []
+    for a in ans:
+        k = ans_map[a]
+        if isinstance(k, str):
+            a_list.append(k)
+        else:
+            tmp = res['备选项' + 'ABCD'[k - 1]]
+            if pd.isna(tmp):
+                # 选择题有答案没选项的情况
+                a_list.append(k)
+            else:
+                a_list.append(tmp)
+    return a_list
 
 
 def click_ans(a_list, div):
@@ -48,38 +68,11 @@ btnSubmit = driver.find_element_by_id('submit_button')
 driver.find_element_by_id("q1").send_keys(name)
 # 公司
 click_ans([company], qs[1])
-# 登录
-btnNext.click()
-# %% 自动答题
-
-
-def get_single_ans(q, answers):
-    """ 返回的是答案列表 """
-    ratio = answers['题干'].apply(lambda s: fuzz.ratio(s, q))
-    res = answers.loc[ratio.idxmax(), :]
-    ans = res['试题答案']
-    a_list = []
-    for a in ans:
-        k = ans_map[a]
-        if isinstance(k, str):
-            a_list.append(k)
-        else:
-            tmp = res['备选项' + 'ABCD'[k - 1]]
-            if pd.isna(tmp):
-                # 选择题有答案没选项的情况
-                a_list.append(k)
-            else:
-                a_list.append(tmp)
-    return a_list
-
-
-# %%
-# 根据选项值搜索, 而不是直接填答案
+# %% 自动答题, 根据选项值搜索, 而不是直接填答案
 for k in range(2, len(qs)):
     div = qs[k]
     if not div.is_displayed():
         btnNext.click()
-        print('下一页...')
     q = div.find_element_by_class_name(
         'div_title_question').text
     q = q.split('\n')[0]
