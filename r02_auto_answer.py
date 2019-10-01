9f702787c3831b8c89cd041d10370c87ccd3607d
鱼泡 <jiangtao.yjt@alibaba-inc.com>
2019-10-02 00:01:11 +0800

    提取参数 一般化操作

--- r02_auto_answer.py
+++ r02_auto_answer.py
@@ -5,6 +5,11 @@ import pandas as pd
 from fuzzywuzzy import fuzz
 from selenium import webdriver
 
+# %%  基本参数
+# url = 'https://ks.wjx.top/jq/45065890.aspx'
+url = 'https://ks.wjx.top/jq/45065834.aspx'
+name = '刘彬'
+company = '分公司本部'
 # %% 加载答案
 answers = pd.read_excel('整理后答案.xlsx', dtype=str)
 # 傻逼 Excel , 这么多乱七八糟的字符
@@ -12,23 +17,39 @@ ans_map = {
     'A': 1, 'B': 2, 'C': 3, 'D': 4, 'Ｂ': 2, 'Ｃ': 3,
     '√': '对', 'x': '错', 'X': '错', 'ｘ': '错', '×': '错'
 }
-# %% 启动和登录
-# 答题链接
-url = 'https://ks.wjx.top/jq/45065890.aspx'
-# url = 'https://ks.wjx.top/jq/45065834.aspx'
+# %% 相关操作的抽象
 
+
+def click_ans(a_list, div):
+    """ 选择对应的选项 """
+    ss = [x.text for x in div.find_elements_by_tag_name('label')]
+    ss = pd.Series(ss)
+    aa = div.find_elements_by_tag_name('a')
+    for a in a_list:
+        if isinstance(a, str):
+            idx = pd.Series(ss).apply(lambda s: fuzz.ratio(s, a)).idxmax()
+        else:
+            idx = a - 1
+            print('WARNING: 有选项没答案, 原选项为 %s , 实际选项为 %s.' %
+                  ('ABCD'[idx], ss.iloc[idx]))
+        aa[idx].click()
+
+
+# %% 启动和登录
 # 启动
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome('./chromedriver', options=options)
 driver.get(url)
-# 姓名
-driver.find_element_by_id("q1").send_keys('刘彬')
-# 公司, 默认选最后一个
-driver.find_element_by_css_selector('a[rel="q2_23"]').click()
-# 登录
-driver.find_element_by_id("btnNext").click()
 # 其实所有题目都在页面上了, 但是只显示了一部分
 qs = driver.find_elements_by_class_name('div_question')
+btnNext = driver.find_element_by_id("btnNext")
+btnSubmit = driver.find_element_by_id('submit_button')
+# 姓名
+driver.find_element_by_id("q1").send_keys(name)
+# 公司
+click_ans([company], qs[1])
+# 登录
+btnNext.click()
 # %% 自动答题
 
 
@@ -37,14 +58,13 @@ def get_single_ans(q, answers):
     ratio = answers['题干'].apply(lambda s: fuzz.ratio(s, q))
     res = answers.loc[ratio.idxmax(), :]
     ans = res['试题答案']
-    ss = '_ABCD'
     a_list = []
     for a in ans:
         k = ans_map[a]
         if isinstance(k, str):
             a_list.append(k)
         else:
-            tmp = res['备选项' + ss[k]]
+            tmp = res['备选项' + 'ABCD'[k - 1]]
             if pd.isna(tmp):
                 # 选择题有答案没选项的情况
                 a_list.append(k)
@@ -58,28 +78,14 @@ def get_single_ans(q, answers):
 for k in range(2, len(qs)):
     div = qs[k]
     if not div.is_displayed():
-        driver.find_element_by_id('btnNext').click()
+        btnNext.click()
         print('下一页...')
     q = div.find_element_by_class_name(
         'div_title_question').text
     q = q.split('\n')[0]
-    ss = [t.text for t in div.find_elements_by_css_selector('label')]
     ans = get_single_ans(q, answers)
     print(q)
     print(ans)
-    ss = pd.Series(ss)
-    for a in ans:
-        if isinstance(a, str):
-            i = ss.apply(lambda s: fuzz.ratio(s, a)).idxmax()
-        else:
-            # 选择题有答案没选项的情况
-            i = a - 1
-            print('WARNING: 有选项没答案, 原选项为 %s , 实际选项为 %s.' %
-                  ('ABCD'[i], ss.iloc[i]))
-        div.find_element_by_css_selector(
-            'a[rel=q%d_%d]' % (k + 1, i + 1)).click()
+    click_ans(ans, div)
 # %% 提交
-driver.find_element_by_id('submit_button').click()
-
-
-# %%
+btnSubmit.click()
